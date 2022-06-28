9063783fbf87c991b486fba3aa5c1822a5bcade2
Everson <eversonpereiraofc@gmail.com>
2022-06-28 12:18:20 -0300

    dinamic and fixed sizes

--- examples_v2/src/App.js
+++ examples_v2/src/App.js
@@ -2,36 +2,22 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import { useState } from 'react';
 
 export default function App() {
-  const [name, setName] = useState("")
-
-  const entrar = (nome) => {
-    setName("Everson")
-  }
-
   return (
-    <View style={[styles.container, styles.area]}>
-      <Text style={styles.textoPrincipal}>Texto 1</Text>
-      <Text>Texto 2</Text>
-      <Text>Texto 3</Text>
-      <Text style={styles.textoPrincipal}>Texto 4</Text>
-    </View>
+    <>
+      <View style={{backgroundColor: "yellow", flex: 1}} />
+      <View style={{backgroundColor: "red", flex: 1}} />
+      <View style={{backgroundColor: "green", flex: 2}} />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ddd',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
-  area: {
-    backgroundColor: 'lightgray'
-  },
-  textoPrincipal: {
-    fontSize: 25,
-    color: 'red'
-  }
 });
 
 
