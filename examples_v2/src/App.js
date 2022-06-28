b807f83d2a34c22c077836e7dc56b421d8eca522
Everson <eversonpereiraofc@gmail.com>
2022-06-28 12:26:34 -0300

    flexbox and align

--- examples_v2/src/App.js
+++ examples_v2/src/App.js
@@ -3,21 +3,22 @@ import { useState } from 'react';
 
 export default function App() {
   return (
-    <>
-      <View style={{backgroundColor: "yellow", flex: 1}} />
-      <View style={{backgroundColor: "red", flex: 1}} />
-      <View style={{backgroundColor: "green", flex: 2}} />
-    </>
+    <View style={styles.container}>
+      <View style={{backgroundColor: "yellow", height: 50, width: 50}} />
+      <View style={{backgroundColor: "red", height: 50, width: 50}} />
+      <View style={{backgroundColor: "green", height: 50, width: 50}} />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: "row",
     backgroundColor: '#ddd',
     alignItems: 'center',
     justifyContent: 'center'
-  },
+  }
 });
 
 
