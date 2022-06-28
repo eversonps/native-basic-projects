19ddef4523f4f9d0ad71081e1e5dd0d2df1661ba
Everson <eversonpereiraofc@gmail.com>
2022-06-28 12:54:21 -0300

    text input with button

--- examples_v2/src/App.js
+++ examples_v2/src/App.js
@@ -3,11 +3,23 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   const [nome, setNome] = useState("")
+  const [text, setText] = useState("")
+
+  const entrar = () => {
+    if(nome.length > 0){
+      setText(`Bem vindo ${nome}`)
+    }else{
+      setText('')
+      alert('Digite seu nome!!!')
+    }
+  }
 
   return (
     <View style={styles.container}>
+      <Text style={styles.texto}>{text}</Text>
       <TextInput style={styles.input} placeholder='Digite seu nome' underlineColorAndroid='transparent' onChangeText={(text) => setNome(text) }/>
-    <Text style={styles.texto}>{nome.length > 0 && `Bem vindo ${nome}`}</Text>
+      
+      <Button onPress={entrar} title="Entrar"></Button>
     </View>
   );
 }
@@ -23,6 +35,7 @@ const styles = StyleSheet.create({
     height: 45,
     width: '80%',
     padding: 5,
+    marginBottom: 20,
     borderWidth: 1,
     borderColor: '#222'
   },
