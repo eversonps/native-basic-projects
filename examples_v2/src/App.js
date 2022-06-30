d5f176764e0834370bf0e335c026b4840cba981b
Everson <eversonpereiraofc@gmail.com>
2022-06-30 13:38:43 -0300

    cronometro

--- examples_v2/src/App.js
+++ examples_v2/src/App.js
@@ -1,73 +1,98 @@
 import { useState } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, TextInput, Image } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native';
 
 export default function App() {
-  const frases = ['Frase 1', 'Frase 2', 'Frase 3', 'Frase 4', 'Frase 5']
+  const [time, setTime] = useState(0)
+  const [timer, setTimer] = useState(null)
+  const [ultimoTempo, setUltimoTempo] = useState(null)
 
-  const [data, setData] = useState({
-    frase: 'Frase 1',
-    img: require('./img/biscoito.png')
-  })
+  const [textoBotao, setTextaoBotao] = useState("VAI")
 
-  const quebraBiscoito = () => {
-    let numeroAleatorio = Math.floor(Math.random() * frases.length)
+  function vai() {
+    if (timer != null) {
+      pausar()
+    } else {
+      let auxTime = time
+      setTextaoBotao("PARAR")
 
-    setData({
-      ...data,
-      frase: frases[numeroAleatorio],
-      img: require('./img/biscoitoAberto.png')
-    })
+      setTimer(setInterval(() => {
+        auxTime++
+        setTime(auxTime)
+      }, 100))
+    }
+  }
+
+  function limpar() {
+    pausar()
+
+    setTime(0)
+  }
+
+  function pausar() {
+    clearInterval(timer)
+    setTimer(null)
+    setTextaoBotao("VAI")
   }
 
   return (
     <View style={styles.container}>
-      <Image style={styles.img} source={data.img} />
-      <Text style={styles.textoFrase}>{data.frase}</Text>
+      <Image source={require('./img/cronometro.png')} style={styles.cronometro} />
 
-      <TouchableOpacity style={styles.botao} onPress={() => quebraBiscoito()}>
-        <View style={styles.botaoArea}>
-          <Text style={styles.botaoTexto}>Quebrar Biscoitos</Text>
-        </View>
-      </TouchableOpacity>
+      <Text style={styles.timer}>{time.toFixed(1)}</Text>
+
+      <View style={styles.btnArea}>
+        <TouchableOpacity style={styles.btn} onPress={vai}>
+          <Text style={styles.btnTexto}>{textoBotao}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btn} onPress={limpar}>
+          <Text style={styles.btnTexto}>LIMPAR</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View>
+        <Text style={styles.ultimoTempo}>{ultimoTempo && `Ultimo tempo: ${time.toFixed(2)}s`}</Text>
+      </View>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#red',
+    backgroundColor: '#00AEEF',
     alignItems: 'center',
     justifyContent: 'center'
   },
-  img: {
-    width: 250,
-    height: 250
-  },
-  textoFrase: {
-    fontSize: 20,
-    color: '#dd7b22',
-    margin: 30,
-    fontStyle: 'italic',
-    textAlign: 'center'
-  },
-  botao: {
-    width: 250,
-    height: 50,
-    borderWidth: 2,
-    borderColor: '#dd7b22',
-    borderRadius: 25
-  },
-  botaoArea: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  botaoTexto: {
-    fontSize: 18,
-    color: '#dd7b22',
+  timer: {
+    marginTop: -160,
+    color: 'white',
+    fontSize: 65,
     fontWeight: 'bold'
+  },
+  btnArea: {
+    flexDirection: 'row',
+    marginTop: 70,
+    height: 40
+  },
+  btn: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+    height: 40,
+    margin: 20,
+    borderRadius: 7
+  },
+  btnTexto: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#00AEEF'
+  },
+  ultimoTempo: {
+    fontSize: 20,
+    color: 'white',
+    marginTop: '20'
   }
 });
 
