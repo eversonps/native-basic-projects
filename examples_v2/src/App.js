13fa6e32badf184a92c41103304ee9a94b096408
Everson <eversonpereiraofc@gmail.com>
2022-06-30 13:54:41 -0300

    ScrollView

--- examples_v2/src/App.js
+++ examples_v2/src/App.js
@@ -1,98 +1,42 @@
 import { useState } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, View, ScrollView } from 'react-native';
 
 export default function App() {
-  const [time, setTime] = useState(0)
-  const [timer, setTimer] = useState(null)
-  const [ultimoTempo, setUltimoTempo] = useState(null)
-
-  const [textoBotao, setTextaoBotao] = useState("VAI")
-
-  function vai() {
-    if (timer != null) {
-      pausar()
-    } else {
-      let auxTime = time
-      setTextaoBotao("PARAR")
-
-      setTimer(setInterval(() => {
-        auxTime++
-        setTime(auxTime)
-      }, 100))
-    }
-  }
-
-  function limpar() {
-    pausar()
-
-    setTime(0)
-  }
-
-  function pausar() {
-    clearInterval(timer)
-    setTimer(null)
-    setTextaoBotao("VAI")
-  }
-
   return (
-    <View style={styles.container}>
-      <Image source={require('./img/cronometro.png')} style={styles.cronometro} />
-
-      <Text style={styles.timer}>{time.toFixed(1)}</Text>
-
-      <View style={styles.btnArea}>
-        <TouchableOpacity style={styles.btn} onPress={vai}>
-          <Text style={styles.btnTexto}>{textoBotao}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.btn} onPress={limpar}>
-          <Text style={styles.btnTexto}>LIMPAR</Text>
-        </TouchableOpacity>
-      </View>
-
-      <View>
-        <Text style={styles.ultimoTempo}>{ultimoTempo && `Ultimo tempo: ${time.toFixed(2)}s`}</Text>
-      </View>
-    </View>
-
+      <ScrollView showsHorizontalScrollIndicator={false} horizontal={true}>
+        <View style={styles.box1}></View>
+        <View style={styles.box2}></View>
+        <View style={styles.box3}></View>
+        <View style={styles.box4}></View>
+      </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#00AEEF',
     alignItems: 'center',
     justifyContent: 'center'
   },
-  timer: {
-    marginTop: -160,
-    color: 'white',
-    fontSize: 65,
-    fontWeight: 'bold'
+  box1: {
+    backgroundColor: 'red',
+    height: 250,
+    width: 150
   },
-  btnArea: {
-    flexDirection: 'row',
-    marginTop: 70,
-    height: 40
+  box2: {
+    backgroundColor: 'blue',
+    height: 250,
+    width: 150
   },
-  btn: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#FFF',
-    height: 40,
-    margin: 20,
-    borderRadius: 7
+  box3: {
+    backgroundColor: 'green',
+    height: 250,
+    width: 150
   },
-  btnTexto: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#00AEEF'
-  },
-  ultimoTempo: {
-    fontSize: 20,
-    color: 'white',
-    marginTop: '20'
+  box4: {
+    backgroundColor: 'orange',
+    height: 250,
+    width: 150
   }
 });
 
