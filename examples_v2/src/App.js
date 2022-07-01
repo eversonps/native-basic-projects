760fe5b89ee2a06e40458e34e18ed085694bdba6
Everson <eversonpereiraofc@gmail.com>
2022-07-01 14:19:52 -0300

    SujeitoInsta

--- examples_v2/src/App.js
+++ examples_v2/src/App.js
@@ -1,90 +1,68 @@
 import { useState } from 'react';
-import { StyleSheet, View, Text, Switch, TextInput, Button } from 'react-native';
+import { StyleSheet, View, Text, Switch, TextInput, Button, Image, TouchableOpacity, FlatList } from 'react-native';
 
-import Slider from '@react-native-community/slider';
-import { Picker } from '@react-native-picker/picker';
-
+import Lista from './components/Lista';
 export default function App() {
-  const [data, setData] = useState({
-    nome: '',
-    idade: 0,
-    sexo: 'M',
-    limit: 600,
-    isStudent: false
-  })
-
-  function enviar(){
-    if(!data.nome){
-      alert('O campo nome é obrigatório!')
-      return
+  const [feed, setFeed] = useState([
+    {
+      id: '1',
+      nome: 'Everson',
+      descricao: 'Ótimo dia! :)',
+      imgPerfil: 'https://www.amambainoticias.com.br/wp-content/uploads/media/images/2231/141495/5ee0ec7b76ae87904f4309c53aae5b5311d75b2d1a433.jpg',
+      imgPublicacao: 'https://pbs.twimg.com/profile_images/1511742442973929484/BDUtYAvu_400x400.jpg',
+      like: false,
+      qtdLike: 0
+    },
+    {
+      id: '2',
+      nome: 'Everson',
+      descricao: 'Ótimo dia! :)',
+      imgPerfil: 'https://www.amambainoticias.com.br/wp-content/uploads/media/images/2231/141495/5ee0ec7b76ae87904f4309c53aae5b5311d75b2d1a433.jpg',
+      imgPublicacao: 'https://pbs.twimg.com/profile_images/1511742442973929484/BDUtYAvu_400x400.jpg',
+      like: false,
+      qtdLike: 3
     }
-
-    if(!data.idade){
-      alert('O campo idade é obrigatório!')
-      return
-    }
-
-    alert('Voce foi cadastrado com sucesso no nosso banco')
-  }
-
+  ])
   return (
     <View style={styles.container}>
-      <Text style={styles.titulo}>Banco</Text>
-
-      <View style={styles.areaCampos}>
-        <Text>Nome:</Text>
-        <TextInput style={styles.input} placeholder='Insira seu nome' onChangeText={(nome) => setData({ ...data, nome })} value={data.nome} underlineColorAndroid={true} />
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Image source={require('./img/logo.png')} style={styles.logo} />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image source={require('./img/send.png')} style={styles.send} />
+        </TouchableOpacity>
       </View>
 
-      <View style={styles.areaCampos}>
-        <Text>Idade:</Text>
-        <TextInput style={styles.input} keyboardType='numeric' placeholder='Insira sua idade' onChangeText={(idade) => setData({ ...data, idade })} value={data.idade} underlineColorAndroid={true}/>
-      </View>
-
-      <View style={styles.areaCampos}>
-        <Text>Sexo:</Text>
-        <Picker selectedValue={data.sexo} onValueChange={(sexo) => setData({ ...data, sexo })}>
-          <Picker.Item key={0} value={'M'} label={'Masculino'} />
-          <Picker.Item key={1} value={'F'} label={'Feminino'} />
-        </Picker>
-      </View>
-
-      <View style={styles.areaCampos}>
-        <Text>Limite do cartão: R${data.limit.toFixed(2)}</Text>
-        <Slider minimumValue={600} maximumValue={3000} value={data.limit} onValueChange={(limit) => setData({ ...data, limit })} />
-      </View>
-
-      <View style={[styles.areaCampos, styles.areaSwitch]}>
-        <Text>É estudante?</Text>
-        <Switch style={styles.campos} value={data.isStudent} onValueChange={(isStudent) => setData({ ...data, isStudent })}/>
-      </View>
-
-      <Button title='Enviar' onPress={enviar}></Button>
-    </View>
+      <FlatList showsHorizontalScrollIndicator={true}
+        data={feed}
+        keyExtractor={ (item) => item.id }
+        renderItem={({ item }) => <Lista item={item}/>} />
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
   },
-  areaCampos: {
-    width: '80%',
-    marginTop: 20
-  },
-  areaSwitch: {
-    display: 'flex',
-    alignItems: 'center',
+  header: {
+    marginTop: 30,
+    height: 55,
+    backgroundColor: '#FFF',
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 5,
+    borderBottomWidth: 0.2,
+    shadowColor: '#000',
+    elevation: 1
   },
-  titulo: {
-    fontSize: 24,
-    fontWeight: 'bold'
+  logo: {
+
   },
-  input: {
-    padding: 8
+  send: {
+    width: 23,
+    height: 23
   }
 });
