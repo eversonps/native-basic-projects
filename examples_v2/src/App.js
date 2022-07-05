c1abf27897e3c4e85cf8332c0f257d992db9b584
Everson <eversonpereiraofc@gmail.com>
2022-07-05 17:44:58 -0300

    conversor

--- examples_v2/src/App.js
+++ examples_v2/src/App.js
@@ -1,46 +1,24 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, View, Text, Switch, TextInput, Button, Modal, Image, TouchableOpacity, FlatList, Keyboard , ActivityIndicator} from 'react-native';
+import { StyleSheet, View, Text, Switch, TextInput, Button, Modal, Image, TouchableOpacity, FlatList, Keyboard, ActivityIndicator } from 'react-native';
 
 import api from './services/api'
 
+import Conversor from './components/Conversor'
 import Filmes from './components/Filmes';
+
+
 export default function App() {
-  const [filmes, setFilmes] = useState([])
-  const [loading, setLoading] = useState(true)
-
-  useEffect(async () => {
-    const response = await api.get('r-api/?api=filmes')
-    setFilmes(response.data)
-    setLoading(false)
-  }, [])
-
-  function renderData() {
-    if (!loading) {
-      return (
-        <View style={styles.container}>
-          <FlatList data={filmes} keyExtractor={item => item.id.toString()} renderItem={({ item }) => {
-            return <Filmes data={item} />
-          }} />
-        </View >
-      )
-    } else {
-      return (
-        <View>
-          <ActivityIndicator></ActivityIndicator>
-        </View>
-      )
-    }
-  }
-  
   return (
-    <>
-      {renderData()}
-    </>
+    <View style={styles.container}>
+      <Conversor moedaA='USD' moedaB='BRL'/>
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   }
 });
--- examples_v2/src/services/api.js
+++ examples_v2/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://sujeitoprogramador.com/'
+  baseURL: '// https://free.currencyconverterapi.com/api/v5/'
 })
 
 export default api
