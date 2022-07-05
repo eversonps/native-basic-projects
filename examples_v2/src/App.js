559ee11a155055bc4bf5f51ec2799ad3ffb47ada
Everson <eversonpereiraofc@gmail.com>
2022-07-05 16:00:23 -0300

    Modal

--- examples_v2/src/App.js
+++ examples_v2/src/App.js
@@ -1,43 +1,31 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, View, Text, Switch, TextInput, Button, Image, TouchableOpacity, FlatList, Keyboard } from 'react-native';
+import { StyleSheet, View, Text, Switch, TextInput, Button, Modal, Image, TouchableOpacity, FlatList, Keyboard } from 'react-native';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
-export default function App() {
-  const [nome, setNome] = useState('Everson')
-  const [input, setInput] = useState('')
 
-  function gravaNome(){
-    setNome(input)
-    alert('salvo com sucesso!')
-    Keyboard.dismiss()
+import Entrar from './components/Entrar';
+
+export default function App() {
+
+  const [visible, setVisible] = useState(false)
+
+  const entrar = () => {
+    setVisible(true)
   }
 
-  useEffect(async () => {
-    await AsyncStorage.setItem('nome', nome)
-  }, [nome])
-
-  useEffect(async () => {
-    await AsyncStorage.getItem('nome').then((value) => {
-      setNome(value)
-    })
-  }, [])
+  const sair = () => {
+    setVisible(false)
+  }
 
   return (
     <View style={styles.container}>
-      <View style={styles.viewInput}>
-        <TextInput
-          style={styles.input}
-          value={input}
-          underlineColorAndroid="transparent"
-          onChangeText={(text) => setInput(text)}
-        />
+      <Button title='Entrar' onPress={entrar} />
 
-        <TouchableOpacity onPress={gravaNome}>
-          <Text style={styles.botao}>+</Text>
-        </TouchableOpacity>
-      </View>
-
-      <Text style={styles.nome}>{nome}</Text>
+      <Modal animationType='slide' visible={visible} transparent={true}>
+        <View style={{margin: 15, flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+          <Entrar sair={sair} />
+        </View>
+      </Modal>
     </View >
   );
 }
@@ -45,28 +33,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  viewInput: {
-    marginTop: 40,
-    flexDirection: 'row',
-    alignItems: 'center'
-  },
-  input: {
-    width: 350,
-    height: 40, 
-    borderColor: '#000',
-    borderWidth: 1,
-    padding: 10
-  },
-  botao: {
-    backgroundColor: '#222',
-    color: '#FFF',
-    height: 40,
-    padding: 10
-  },
-  nome: {
-    marginTop: 15,
-    fontSize: 30,
-    textAlign: 'center'
   }
 });
