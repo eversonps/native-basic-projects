b40cce9b13fe25fe0f0b99d4f99c8221cff961e7
Everson <eversonpereiraofc@gmail.com>
2022-07-19 09:55:18 -0300

    icons

--- navegation/App.js
+++ navegation/App.js
@@ -16,12 +16,22 @@ import {
   Text,
   useColorScheme,
   View,
+  TouchableOpacity
 } from 'react-native';
+
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Text style={{textAlign: "center"}}>Ola, ola mundo</Text>
+      <FontAwesome name="home" size={25} color="#11118c" />
+      <FontAwesome name="user" size={25} color="#54a300" />
+      <TouchableOpacity style={styles.btnYoutube}>
+        <FontAwesome name="youtube" size={25} color="#FFF" />
+        <Text style={styles.btnText}>Acessar canal</Text>
+      </TouchableOpacity>
     </SafeAreaView>
   );
 };
@@ -31,6 +41,17 @@ const styles = StyleSheet.create({
     flex: 1,
     alignContent: "center",
     justifyContent: "center"
+  },
+  btnYoutube: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'red',
+    borderRadius: 5
+  },
+  btnText: {
+    paddingLeft: 5,
+    color: '#FFF'
   }
 });
 
