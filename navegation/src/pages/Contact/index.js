76e7f022b686378d642b00f8ccb313118d7e1ded
Everson <eversonpereiraofc@gmail.com>
2022-07-19 10:49:24 -0300

    navegations

---
+++ navegation/src/pages/Contact/index.js
@@ -0,0 +1,46 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import React from 'react';
+
+ import {
+   SafeAreaView,
+   ScrollView,
+   StatusBar,
+   StyleSheet,
+   Text,
+   useColorScheme,
+   View,
+   TouchableOpacity,
+   Button
+ } from 'react-native';
+ 
+import { useNavigation, StackActions } from '@react-navigation/native';
+ 
+ const Contact = () => {
+  const navigation = useNavigation()
+
+   return (
+     <SafeAreaView style={styles.container}>
+       <Text style={{textAlign: "center"}}>CONTATO</Text>
+       <Button title="Voltar tela anterior" onPress={() => navigation.goBack()}/>  
+       <Button title="Voltar home" onPress={() => navigation.dispatch(StackActions.popToTop())}/> 
+     </SafeAreaView>
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     alignContent: "center",
+     justifyContent: "center"
+   }
+ });
+ 
+ export default Contact;
+ 
---
+++ navegation/src/pages/Home/index.js
@@ -0,0 +1,53 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import React from 'react';
+
+ import {
+   SafeAreaView,
+   ScrollView,
+   StatusBar,
+   StyleSheet,
+   Text,
+   useColorScheme,
+   View,
+   TouchableOpacity,
+   Button
+ } from 'react-native';
+ 
+ import FontAwesome from 'react-native-vector-icons/FontAwesome';
+ import Feather from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+ 
+ const Home = () => {
+   const navigation = useNavigation()
+
+
+   function navegaSobre(){
+     navigation.navigate("Sobre", {nome: "Everson", email: "eversonpereiraofc@gmail.com"})
+   }
+
+   return (
+     <SafeAreaView style={styles.container}>
+       <Text style={{textAlign: "center"}}>TELA HOME</Text>
+       <Button title="Ir para sobre" onPress={() => navegaSobre()}/>
+
+     </SafeAreaView>
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     alignContent: "center",
+     justifyContent: "center"
+   }
+ });
+ 
+ export default Home;
+ 
