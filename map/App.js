a93334610cd2f5165e11da45f5dcfb21cb5da7dc
Everson <eversonpereiraofc@gmail.com>
2022-07-22 15:58:53 -0300

    current location

--- map/App.js
+++ map/App.js
@@ -8,7 +8,7 @@
 
 // AIzaSyDAMORbQ5M-iCHW_80hQXpOo9WcahKlBtg
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import MapView, { Marker } from 'react-native-maps';
 
 import {
@@ -23,12 +23,7 @@ import {
 } from 'react-native';
 
 export default function App(){
-  const [region, setRegion] = useState({
-    latitude: -23.5492243,
-    longitude: -46.5813785,
-    latitudeDelta: 0.0922,
-    longitudeDelta: 0.0421
-  })
+  const [region, setRegion] = useState(null)
 
   const [markers, setMarkers] = useState([
     {key: 0, coords: {latitude: -15.8080374, longitude: -47.8750231}, image: require('./src/assets/images/carro_left.png')},
@@ -39,22 +34,45 @@ export default function App(){
     setRegion({...region, longitude: regiao.longitude, latitude: regiao.latitude})
   }
 
+  useEffect(() => {
+
+    async function loadMapa(){
+      await navigator.geolocation.getCurrentPosition(async ({ coords: {latitude, longitude} }) => {
+        setRegion({
+          latitude: latitude,
+          longitude: longitude,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421
+        })
+      },
+      () => { alert("erro")},
+      {
+        timeout: 2000,
+        maximumAge: 1000
+      }
+      )
+    }
+
+    loadMapa()
+    
+  }, [])
   return (
     <SafeAreaView style={styles.container}>
       <View style={{flexDirection: 'row', width: '100%', justifyContent: 'space-around', marginBottom: 20}}>
         <Button title='Brasilia' onPress={() => setRegion({...region, latitude: -15.8080374, longitude: -47.8750231})}/>
         <Button title='Sao Paulo' onPress={() => setRegion({...region, latitude: -23.5492243, longitude: -46.5813785})}/>
       </View>
-      <MapView style={styles.mapa}
+      { region && 
+       <MapView style={styles.mapa}
+       minZoomLevel={16}
+       showsUserLocation
+       loadingEnabled
        region={region} 
        onRegionChangeComplete={changedMap} 
-       onPress={(e) => alert('Latitude Clicada: ' + e.nativeEvent.coordinate.latitude)}
-      > 
-        {markers.map(marker => {
-          return <Marker image={marker.image} coordinate={marker.coords} key={marker.key} /* pinColor={marker.pinColor} */ />
-        })
-        }
-      </MapView>
+       onPress={(e) => alert('Latitude Clicada: ' + e.nativeEvent.coordinate.latitude)}      
+       /> }
+     
+
     </SafeAreaView>
   );
 };
