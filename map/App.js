968dadb627d02bd9d09ee85e9f7f5c15d4de9066
Everson <eversonpereiraofc@gmail.com>
2022-07-22 16:48:29 -0300

    fix current location

--- map/App.js
+++ map/App.js
@@ -11,6 +11,8 @@
 import React, { useEffect, useState } from 'react';
 import MapView, { Marker } from 'react-native-maps';
 
+import Geolocation from '@react-native-community/geolocation';
+
 import {
   SafeAreaView,
   ScrollView,
@@ -23,7 +25,7 @@ import {
 } from 'react-native';
 
 export default function App(){
-  const [region, setRegion] = useState(null)
+  const [region, setRegion] = useState()
 
   const [markers, setMarkers] = useState([
     {key: 0, coords: {latitude: -15.8080374, longitude: -47.8750231}, image: require('./src/assets/images/carro_left.png')},
@@ -35,42 +37,42 @@ export default function App(){
   }
 
   useEffect(() => {
-
-    async function loadMapa(){
-      await navigator.geolocation.getCurrentPosition(async ({ coords: {latitude, longitude} }) => {
-        setRegion({
-          latitude: latitude,
-          longitude: longitude,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421
-        })
-      },
-      () => { alert("erro")},
-      {
-        timeout: 2000,
-        maximumAge: 1000
-      }
-      )
+    async function loadCurrentLocation(){
+      await Geolocation.getCurrentPosition(
+        (pos) => {
+          setRegion({
+            latitude: pos.coords.latitude,
+            longitude: pos.coords.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421
+          })
+        },
+        () => { alert("erro")},
+        {
+          timeout: 2000,
+          maximumAge: 1000
+        }
+      )   
     }
 
-    loadMapa()
+    loadCurrentLocation()
     
   }, [])
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={{flexDirection: 'row', width: '100%', justifyContent: 'space-around', marginBottom: 20}}>
         <Button title='Brasilia' onPress={() => setRegion({...region, latitude: -15.8080374, longitude: -47.8750231})}/>
         <Button title='Sao Paulo' onPress={() => setRegion({...region, latitude: -23.5492243, longitude: -46.5813785})}/>
       </View>
-      { region && 
-       <MapView style={styles.mapa}
-       minZoomLevel={16}
-       showsUserLocation
-       loadingEnabled
-       region={region} 
-       onRegionChangeComplete={changedMap} 
-       onPress={(e) => alert('Latitude Clicada: ' + e.nativeEvent.coordinate.latitude)}      
-       /> }
+
+
+       <MapView
+        showsUserLocation
+        loadingEnabled
+        style={styles.mapa}
+        region={region} 
+       />
      
 
     </SafeAreaView>
