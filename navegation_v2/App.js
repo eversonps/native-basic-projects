75d7dcec3c3ed33fe8f38977508a2f263f19bfb5
Everson <eversonpereiraofc@gmail.com>
2022-07-26 10:52:27 -0300

    initializing navegation v2 project

---
+++ navegation_v2/App.js
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native'; 
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Ola mundo!</Text>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+});
